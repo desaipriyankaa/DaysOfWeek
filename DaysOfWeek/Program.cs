1147edbb72335773c6d49cb636163e7416a93ad9
desaipriyankaa <priyanka.desai@klingelnberg.com>
2021-04-18 13:20:58 +0530

    Add project files.

---
+++ DaysOfWeek/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DaysOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] DaysOfWeeks = {
+                                    "Monday",
+                                    "Tuesday",
+                                    "Wednesday",
+                                    "Thursday",
+                                    "Friday",
+                                    "Satursday",
+                                    "Sunday"
+                                  };
+
+            foreach (string day in DaysOfWeeks)
+            {
+                Console.WriteLine(day);
+            }
+        }
+
+    }
+}
